469ab713abebddd8c5438fd4b3f15008df5dcf8f
fulltuajing <fulltuajing@gmail.com>
2026-08-28 14:40:32 +0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "นายห้างเองครับ🙏",     // this is what people actually see
+        state: "WEEEDMAN",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
